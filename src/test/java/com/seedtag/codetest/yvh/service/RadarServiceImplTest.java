ec1dc2b7fe7f48ad89dc5bfdb86c3174f13a7561
ricardo <richygp@gmail.com>
2022-06-25 21:59:09 +0200

    Defines Controller unit tests.

---
+++ src/test/java/com/seedtag/codetest/yvh/service/RadarServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.seedtag.codetest.yvh.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadarServiceImplTest {
+
+    @Test
+    void getAttackCoordinates() {
+    }
+}
